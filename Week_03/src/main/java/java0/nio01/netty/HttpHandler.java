fbf64936c85a604264ebb40ca646b45c4e300963
孙瑜 <dbsunyu@travelsky.com>
2021-01-22 16:27:15 +0800

    第san作业

---
+++ Week_03/src/main/java/java0/nio01/netty/HttpHandler.java
@@ -0,0 +1,93 @@
+package java0.nio01.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx);
+            }
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+        try {
+//            String value = "hello,kimmking";
+            String value =null;
+            //打开浏览器
+            CloseableHttpClient httpClient= HttpClients.createDefault();
+            //声明get请求
+            HttpGet httpGet=new HttpGet("http://localhost:8808");
+            //3.发送请求
+            CloseableHttpResponse response1 = httpClient.execute(httpGet);
+            if(response1.getStatusLine().getStatusCode()==200){
+                HttpEntity entity=response1.getEntity();
+                 value = EntityUtils.toString(entity);
+//                System.out.println(content);
+            }
+            response1.close();
+            httpClient.close();
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            System.out.println("处理出错:"+e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
